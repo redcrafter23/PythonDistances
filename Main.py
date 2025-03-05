dbb713e3eeb16a73aa3cb412d2286518a110128b
redcrafter23 <jonathan.friedrich.groove@gmail.com>
2025-03-05 13:28:15 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,20 @@
+import math
+
+points = {}
+distance = {}
+dist = 0
+
+dimensions = int(input("Input number of dimensions:\n"))
+for i in range(1,3):
+    for j in range(1 ,(dimensions+1)):
+        position = int(input("Input Point %s dimension %s value:\n" % (i,j)))
+        points.update({"p"+ str(i) + str(j): position})
+        if i == 2:
+            distance.update({str(j): abs(abs(points["p1" + str(j)])-abs(points["p2" + str(j)]))})
+
+for i in range(1,(dimensions+1)):
+    dist = dist + (distance[str(i)] * distance[str(i)])
+
+
+dist = math.sqrt(dist)
+print(dist)
